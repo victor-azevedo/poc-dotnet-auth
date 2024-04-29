fd5f9a00f131a80e02988275b0546529995673f0
Victor Azevedo <josevcazevedo@gmail.com>
2024-04-29 20:53:57 -0300

    feat: add JWT Auth

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using PocAuth.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
